77372f58e4a4870b1687ada5729f5d18de0ee446
Denys Sene <denys.sene@gmail.com>
2024-08-30 04:06:50 -0300

    Updates for Release 0.2.0

--- src/SimpleKalmanFilter.cpp
+++ src/SimpleKalmanFilter.cpp
@@ -10,40 +10,42 @@
 
 SimpleKalmanFilter::SimpleKalmanFilter(float mea_e, float est_e, float q)
 {
-  _err_measure=mea_e;
-  _err_estimate=est_e;
+  _err_measure = mea_e;
+  _err_estimate = est_e;
   _q = q;
 }
 
 float SimpleKalmanFilter::updateEstimate(float mea)
 {
-  _kalman_gain = _err_estimate/(_err_estimate + _err_measure);
+  _kalman_gain = _err_estimate / (_err_estimate + _err_measure);
   _current_estimate = _last_estimate + _kalman_gain * (mea - _last_estimate);
-  _err_estimate =  (1.0f - _kalman_gain)*_err_estimate + fabsf(_last_estimate-_current_estimate)*_q;
-  _last_estimate=_current_estimate;
+  _err_estimate = (1.0f - _kalman_gain) * _err_estimate + fabsf(_last_estimate - _current_estimate) * _q;
+  _last_estimate = _current_estimate;
 
   return _current_estimate;
 }
 
 void SimpleKalmanFilter::setMeasurementError(float mea_e)
 {
-  _err_measure=mea_e;
+  _err_measure = mea_e;
 }
 
 void SimpleKalmanFilter::setEstimateError(float est_e)
 {
-  _err_estimate=est_e;
+  _err_estimate = est_e;
 }
 
 void SimpleKalmanFilter::setProcessNoise(float q)
 {
-  _q=q;
+  _q = q;
 }
 
-float SimpleKalmanFilter::getKalmanGain() {
+float SimpleKalmanFilter::getKalmanGain()
+{
   return _kalman_gain;
 }
 
-float SimpleKalmanFilter::getEstimateError() {
+float SimpleKalmanFilter::getEstimateError()
+{
   return _err_estimate;
 }
--- src/SimpleKalmanFilter.h
+++ src/SimpleKalmanFilter.h
@@ -26,7 +26,6 @@ private:
   float _current_estimate = 0;
   float _last_estimate = 0;
   float _kalman_gain = 0;
-
 };
 
 #endif
